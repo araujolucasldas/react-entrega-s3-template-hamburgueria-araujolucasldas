8ec5fa4d27f804fcf52fb33f6ec319744f844508
araujolucasldas <araujo.lucas.ldas@gmail.com>
2024-01-19 12:39:46 -0300

    commit de correção

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -3,13 +3,19 @@ import { CartModal } from "../../components/CartModal";
 import { Header } from "../../components/Header";
 import { ProductList } from "../../components/ProductList";
 
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css'
+
 export const HomePage = () => {
    const [productList, setProductList] = useState([]);
    const localCartItems = localStorage.getItem("@CARTITEMS")
 
    const [cartList, setCartList] = useState(localCartItems ? JSON.parse(localCartItems) : []);
 
-   // useEffect montagem - carrega os produtos da API e joga em productList
+   const notify = ()=>{
+      toast.info("Você já adicionou este item ao carrinho")
+  }
+
    useEffect(()=>{
       const getProducts = async ()=>{
          const response = await fetch("https://hamburgueria-kenzie-json-serve.herokuapp.com/products")
@@ -19,18 +25,16 @@ export const HomePage = () => {
       getProducts()
    },[])
    
-   // useEffect atualização - salva os produtos no localStorage (carregar no estado)
    useEffect(()=>{
       localStorage.setItem("@CARTITEMS", JSON.stringify(cartList))
    },[cartList])
 
-   // adição, exclusão, e exclusão geral do carrinho
    const addToCart = (item)=>{
       const verifyItem = cartList.some((cartItem)=> cartItem.id === item.id)
       if(!verifyItem){
         setCartList([...cartList, item])
-      console.log(cartList) 
       }else{
+         notify()
       }
    }
 
@@ -43,12 +47,7 @@ export const HomePage = () => {
       setCartList([])
    }
 
-   // renderizações condições e o estado para exibir ou não o carrinho
-
    const [openCart, setOpenCart] = useState(false)
-
-   // filtro de busca
-   // estilizar tudo com sass de forma responsiva
 
    return (
       <>
@@ -58,6 +57,7 @@ export const HomePage = () => {
             {openCart ? <CartModal cartList={cartList} removeFromCart={removeFromCart} removeAllItems={removeAllItems} setOpenCart={setOpenCart} /> : null}
             
          </main>
+         <ToastContainer />
       </>
    );
 };
